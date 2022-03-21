350533991d53831923d362db9b639fd8964bef1a
Mateo Echavarria Sierra <echavarria.0042@gmail.com>
2022-03-20 20:52:42 -0500

    Create Asiento.java

---
+++ src/Asiento.java
@@ -0,0 +1,12 @@
+package test;
+public class Asiento {
+    String color;
+    int precio;
+    int registro;
+
+    void cambiarColor(String color) {
+        if (color==("amarillo")||(color=="rojo")||(color=="verde")||(color=="negro")||(color=="blanco")){
+            this.color=color;
+        }
+    }
+}
